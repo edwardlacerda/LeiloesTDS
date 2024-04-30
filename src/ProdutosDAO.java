a43b366e1d9350dc90bd1016be3634431252637a
Edward Lacerda <elacerda28@gmail.com>
2024-04-30 20:54:06 -0300

    commit #1: botão de cadastrar salvando o produto no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,45 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
-    Connection conn;
+
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public static boolean cadastrarProduto(ProdutosDTO produto) {
+        try {
+            conectaDAO conexao = new conectaDAO();
+            conexao.conectar();
+
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?);";
+
+            PreparedStatement query = conexao.getConexao().prepareStatement(sql);
+
+            query.setString(1, produto.getNome());
+            query.setDouble(2, produto.getValor());
+            query.setString(3, produto.getStatus());
+            query.execute();
+
+            conexao.desconectar();
+            return true;
+        } catch (SQLException se) {
+            System.out.println("Erro ao executar a consulta: " + se.getMessage());
+            return false;
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -2,32 +2,46 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
-
 
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
-        return conn;
+
+    private Connection conexao;
+
+    public Connection getConexao() {
+        return conexao;
     }
-    
+
+    private final String url = "jdbc:mysql://localhost:3306/uc11?useSSL=false";
+    private final String user = "root";
+    private final String password = "root";
+
+    public void conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection(url, user, password);
+            System.out.println("Conectado!");
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+
+        }
+    }
+
+    public void desconectar() {
+        try {
+            conexao.close();
+        } catch (SQLException ex) {
+
+        }
+    }
+
 }
